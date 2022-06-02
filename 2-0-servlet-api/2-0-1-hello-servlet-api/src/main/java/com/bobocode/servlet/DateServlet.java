b6b49b1022370eb8886072ec2ecf3691a83f1dfc
Shumyk <dima.shumyk@gmail.com>
2022-06-02 17:35:27 +0300

    2-0-1: hello servlet api exercise

---
+++ 2-0-servlet-api/2-0-1-hello-servlet-api/src/main/java/com/bobocode/servlet/DateServlet.java
@@ -0,0 +1,22 @@
+package com.bobocode.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+@WebServlet("/date")
+public class DateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+
+        final var writer = resp.getWriter();
+        writer.println(LocalDate.now());
+        writer.flush();
+    }
+}
